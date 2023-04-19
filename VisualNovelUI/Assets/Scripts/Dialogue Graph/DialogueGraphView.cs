63cd24c9ff5821c54d254c363197420483a56a63
Nicholas Ross <RossNicholasV@gmail.com>
2023-04-19 05:42:55 -0500

    Name, Save, Load
    --Added Folder "Scripts/Runtime"
    --Setup base runtime classes for nodes and their links
    --Moved Dialogue Graph scrips to "Scripts/Dialogue Graph"
    --Added Naming System
    --Added Save System
    --Added Load System
    --Node outputs can be labeled
    --Node outputs can be deleted

---
+++ VisualNovelUI/Assets/Scripts/Dialogue Graph/DialogueGraphView.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DialogueGraphView : GraphView
+{
+    public readonly Vector2 defaultNodeSize = new Vector2(150, 200);
+    public DialogueGraphView()
+    {
+        styleSheets.Add(Resources.Load<StyleSheet>("DialogueGraph"));
+        SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+
+        var grid = new GridBackground();
+        Insert(0, grid);
+        grid.StretchToParentSize();
+
+        AddElement(GenerateEntryPointNode());
+    }
+
+    public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+    {
+        var compatiblePorts = new List<Port>();
+
+        ports.ForEach((port) =>
+        {
+            if(startPort != port && startPort.node != port.node)
+                compatiblePorts.Add(port);
+        });
+
+        return compatiblePorts;
+    } 
+
+    private Port GeneratePort(DialogueNode node, Direction portDirection, Port.Capacity capacity=Port.Capacity.Single)
+    {
+        return node.InstantiatePort(Orientation.Horizontal, portDirection, capacity, typeof(float));
+    }
+
+    private DialogueNode GenerateEntryPointNode()
+    {
+        var node = new DialogueNode
+        {
+            title = "Start",
+            GUID = Guid.NewGuid().ToString(),
+            dialogueText = "Entry Point",
+            entryPoint = true
+        };
+
+        var generatedPort = GeneratePort(node, Direction.Output);
+        generatedPort.portName = "Next";
+        node.outputContainer.Add(generatedPort);
+
+        node.capabilities &= ~Capabilities.Movable;
+        node.capabilities &= ~Capabilities.Deletable;
+
+        node.RefreshExpandedState();
+        node.RefreshPorts();
+
+        node.SetPosition(new Rect(x: 100, y: 200, width: 100, height: 150));
+
+        return node;
+    }
+
+    public void CreateNode(string nodeName)
+    {
+        AddElement(CreateDialogueNode(nodeName));
+    }
+
+    public DialogueNode CreateDialogueNode(string nodeName)
+    {
+        var dialogueNode = new DialogueNode
+        {
+            title = nodeName,
+            dialogueText = nodeName,
+            GUID = Guid.NewGuid().ToString()
+        };
+
+        var inputPort = GeneratePort(dialogueNode, Direction.Input, Port.Capacity.Multi);
+        inputPort.portName = "Input";
+        dialogueNode.inputContainer.Add(inputPort);
+
+        dialogueNode.styleSheets.Add(Resources.Load<StyleSheet>("Node"));
+
+        var button = new Button (() => {AddChoicePort(dialogueNode);});
+        button.text = "New Choice";
+        dialogueNode.titleContainer.Add(button);
+
+        var textField = new TextField(string.Empty);
+        textField.RegisterValueChangedCallback(evt =>
+        {
+            dialogueNode.dialogueText = evt.newValue;
+            dialogueNode.title = evt.newValue;
+        });
+        textField.SetValueWithoutNotify(dialogueNode.title);
+        dialogueNode.mainContainer.Add(textField);
+
+        dialogueNode.RefreshExpandedState();
+        dialogueNode.RefreshPorts();
+
+        dialogueNode.SetPosition(new Rect(Vector2.zero, defaultNodeSize));
+
+        return dialogueNode;
+    }
+
+    public void AddChoicePort(DialogueNode dialogueNode, string overridenPortName = "")
+    {
+        var generatedPort = GeneratePort(dialogueNode, Direction.Output, Port.Capacity.Single);
+
+        var oldLabel = generatedPort.contentContainer.Q<Label>("type");
+        generatedPort.contentContainer.Remove(oldLabel);
+
+        var outputPortCount = dialogueNode.outputContainer.Query("connector").ToList().Count;
+
+        var choicePortName = string.IsNullOrEmpty(overridenPortName) 
+            ? $"Choice {outputPortCount}"
+            : overridenPortName;
+
+        var textField = new TextField
+        {
+            name = string.Empty,
+            value = choicePortName
+        };
+        textField.RegisterValueChangedCallback(evt => generatedPort.portName = evt.newValue);
+        generatedPort.contentContainer.Add(new Label("  "));
+        generatedPort.contentContainer.Add(textField);
+
+        var deleteButton = new Button(() => RemovePort(dialogueNode, generatedPort))
+        {
+            text = "X"
+        };
+        generatedPort.contentContainer.Add(deleteButton);
+
+        generatedPort.portName = choicePortName;
+        dialogueNode.outputContainer.Add(generatedPort);
+        dialogueNode.RefreshPorts();
+        dialogueNode.RefreshExpandedState();
+    }
+
+    private void RemovePort(DialogueNode dialogueNode, Port generatedPort)
+    {
+        var targetEdge = edges.ToList().Where(x => 
+            x.output.portName == generatedPort.portName && x.output.node == generatedPort.node);
+
+        if (targetEdge.Any()) 
+        {
+            var edge = targetEdge.First();
+            edge.input.Disconnect(edge);
+            RemoveElement(targetEdge.First());
+        }
+
+        dialogueNode.outputContainer.Remove(generatedPort);
+        dialogueNode.RefreshPorts();
+        dialogueNode.RefreshExpandedState();
+    }
+}
---
+++ VisualNovelUI/Assets/Scripts/Dialogue Graph/GraphSaveUtility.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GraphSaveUtility
+{
+    private DialogueGraphView _targetGraphView;
+    private DialogueContainer _containerCache;
+
+    private List<Edge> Edges => _targetGraphView.edges.ToList();
+    private List<DialogueNode> Nodes => _targetGraphView.nodes.ToList().Cast<DialogueNode>().ToList();
+
+    public static GraphSaveUtility GetInstance(DialogueGraphView targetGraphView)
+    {
+        return new GraphSaveUtility
+        {
+            _targetGraphView = targetGraphView
+        };
+    }
+
+    public void SaveGraph(string fileName)
+    {
+        if (!Edges.Any()) return;
+
+        var dialogueContainer = ScriptableObject.CreateInstance<DialogueContainer>();
+
+        var connectedPorts = Edges.Where(x => x.input.node != null).ToArray();
+        for (int i = 0; i < connectedPorts.Length; i++)
+        {
+            var outputNode = connectedPorts[i].output.node as DialogueNode;
+            var inputNode = connectedPorts[i].input.node as DialogueNode;
+
+            dialogueContainer.NodeLinks.Add(new NodeLinkData
+            {
+                BaseNodeGuid = outputNode.GUID,
+                PortName = connectedPorts[i].output.portName,
+                TargetNodeGuid = inputNode.GUID
+            });
+        }
+
+        foreach (var dialogueNode in Nodes.Where(node => !node.entryPoint))
+        {
+            dialogueContainer.DialogueNodeData.Add(new DialogueNodeData
+            {
+                GUID = dialogueNode.GUID,
+                DialogueText = dialogueNode.dialogueText,
+                Position = dialogueNode.GetPosition().position
+            });
+        }
+
+        if(!AssetDatabase.IsValidFolder("Assets/Resources"))
+            AssetDatabase.CreateFolder("Assets", "Resources");
+
+        AssetDatabase.CreateAsset(dialogueContainer,  $"Assets/Resources/{fileName}.asset");
+        AssetDatabase.SaveAssets();
+    }
+
+    public void LoadGraph(string fileName)
+    {
+        _containerCache = Resources.Load<DialogueContainer>(fileName);
+        if (_containerCache == null)
+        {
+            EditorUtility.DisplayDialog("File Not Found", "Target dialogue graph file not fount", "OK");
+            return;
+        }
+        
+        ClearGraph();
+        CreateNodes();
+        ConnectNodes();
+    }
+
+    private void ClearGraph()
+    {
+        Nodes.Find(x => x.entryPoint).GUID = _containerCache.NodeLinks[0].BaseNodeGuid;
+
+        foreach (var node in Nodes)
+        {
+            if (node.entryPoint) continue;
+            Edges.Where(x => x.input.node == node).ToList().ForEach(edge => _targetGraphView.RemoveElement(edge));
+
+            _targetGraphView.RemoveElement(node);
+        }
+    }
+
+    private void CreateNodes()
+    {
+        foreach (var nodeData in _containerCache.DialogueNodeData)
+        {
+            var tempNode = _targetGraphView.CreateDialogueNode(nodeData.DialogueText);
+            tempNode.GUID = nodeData.GUID;
+            _targetGraphView.AddElement(tempNode);
+
+            var nodePorts = _containerCache.NodeLinks.Where(x => x.BaseNodeGuid == nodeData.GUID).ToList();
+            nodePorts.ForEach(x => _targetGraphView.AddChoicePort(tempNode, x.PortName));
+        }
+    }
+
+    private void ConnectNodes()
+    {
+        for (var i = 0; i < Nodes.Count; i++)
+            {
+                var k = i; //Prevent access to modified closure
+                var connections = _containerCache.NodeLinks.Where(x => x.BaseNodeGuid == Nodes[k].GUID).ToList();
+                for (var j = 0; j < connections.Count(); j++)
+                {
+                    var targetNodeGUID = connections[j].TargetNodeGuid;
+                    var targetNode = Nodes.First(x => x.GUID == targetNodeGUID);
+                    LinkNodes(Nodes[i].outputContainer[j].Q<Port>(), (Port) targetNode.inputContainer[0]);
+
+                    targetNode.SetPosition(new Rect(
+                        _containerCache.DialogueNodeData.First(x => x.GUID == targetNodeGUID).Position,
+                        _targetGraphView.defaultNodeSize));
+                }
+            }
+    }
+
+    private void LinkNodes(Port output, Port input)
+    {
+        var tempEdge = new Edge
+        {
+            output = output,
+            input = input
+        };
+
+        tempEdge?.input.Connect(tempEdge);
+        tempEdge?.output.Connect(tempEdge);
+        _targetGraphView.Add(tempEdge);
+    }
+}
