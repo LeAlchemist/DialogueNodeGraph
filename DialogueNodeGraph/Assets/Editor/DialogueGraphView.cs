8ef62f29fdfbab6ab9e9f21e355c9bbc5ba3a1a6
Nicholas Ross <RossNicholasV@gmail.com>
2023-04-28 01:03:32 -0500

    SCRIPTABLE NODES
    Creating nodes, placing them on the graph, and linking them work.

--- DialogueNodeGraph/Assets/Editor/DialogueGraphView.cs
+++ DialogueNodeGraph/Assets/Editor/DialogueGraphView.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine.UIElements;
 using UnityEditor.Experimental.GraphView;
+using System;
+using System.Linq;
 
 public class DialogueGraphView : GraphView
 {
     public new class UxmlFactory : UxmlFactory<DialogueGraphView, GraphView.UxmlTraits>{}
+    DialogueGraph graph;
 
     public DialogueGraphView()
     {
@@ -17,7 +20,115 @@ public class DialogueGraphView : GraphView
         this.AddManipulator(new SelectionDragger());
         this.AddManipulator(new RectangleSelector());
 
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/DialogueTreeEditor.uss");
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/DialogueGraphEditor.uss");
         styleSheets.Add(styleSheet);
+    }
+
+    NodeView FindNodeView (Node node)
+    {
+        return GetNodeByGuid(node.guid) as NodeView;
+    }
+
+    internal void PopulateView(DialogueGraph graph)
+    {
+        this.graph = graph;
+
+        graphViewChanged -= OnGraphViewChanged;
+        DeleteElements(graphElements);
+        graphViewChanged += OnGraphViewChanged;
+
+        // creates the node views on graph
+        graph.nodes.ForEach(n => CreateNodeView(n));
+
+        // creates the node edges
+        graph.nodes.ForEach(n => {
+            var children = graph.GetChildren(n);
+            children.ForEach(c => {
+                NodeView parentView = FindNodeView(n);
+                NodeView childView = FindNodeView(c);
+
+                Edge edge = parentView.output.ConnectTo(childView.input);
+                AddElement(edge);
+            });
+        });
+    }
+
+    public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+    {
+        return ports.ToList().Where(endPort => endPort.direction != startPort.direction && endPort.node != startPort.node).ToList();
+    }
+
+    private GraphViewChange OnGraphViewChanged(GraphViewChange graphViewChange)
+    {
+        if(graphViewChange.elementsToRemove != null)
+        {
+            graphViewChange.elementsToRemove.ForEach(elem => {
+                NodeView nodeView = elem as NodeView;
+                if (nodeView != null)
+                {
+                    graph.DeleteNode(nodeView.node);
+                }
+
+                Edge edge = elem as Edge;
+                if (edge != null)
+                {
+                    NodeView parentView = edge.output.node as NodeView;
+                    NodeView childView = edge.input.node as NodeView;
+                    graph.RemoveChild(parentView.node, childView.node);
+                }
+            });
+        }
+
+        if(graphViewChange.edgesToCreate != null)
+        {
+            graphViewChange.edgesToCreate.ForEach(edge =>
+            {
+                NodeView parentView = edge.output.node as NodeView;
+                NodeView childView = edge.input.node as NodeView;
+                graph.AddChild(parentView.node, childView.node);
+            });
+        }
+
+        return graphViewChange;
+    }
+
+    public override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
+    {
+        //base.BuildContextualMenu(evt);
+        {
+            var types = TypeCache.GetTypesDerivedFrom<ActionNode>();
+            foreach (var type in types)
+            {
+                evt.menu.AppendAction($"[{type.BaseType.Name}] {type.Name}", (a) => CreateNode(type));
+            }
+        }
+
+        {
+            var types = TypeCache.GetTypesDerivedFrom<CompositeNode>();
+            foreach (var type in types)
+            {
+                evt.menu.AppendAction($"[{type.BaseType.Name}] {type.Name}", (a) => CreateNode(type));
+            }
+        }
+
+        {
+            var types = TypeCache.GetTypesDerivedFrom<DecoratorNode>();
+            foreach (var type in types)
+            {
+                evt.menu.AppendAction($"[{type.BaseType.Name}] {type.Name}", (a) => CreateNode(type));
+            }
+        }
+    }
+
+    void CreateNode(System.Type type)
+    {
+        Node node = graph.CreateNode(type);
+        CreateNodeView(node);
+    }
+
+    void CreateNodeView(Node node)
+    {
+        NodeView nodeView = new NodeView(node);
+        AddElement(nodeView);
     }
 }
---
+++ DialogueNodeGraph/Assets/Scripts/Dialogue Graph/DialogueGraph.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu()]
+public class DialogueGraph : ScriptableObject
+{
+    public Node rootNode;
+    public Node.State graphState = Node.State.Running;
+    public List<Node> nodes = new List<Node>();
+
+    public Node.State Update()
+    {
+        if (rootNode.state == Node.State.Running)
+        {
+            graphState = rootNode.Update();
+        }
+
+        return graphState;
+    }
+
+    public Node CreateNode(System.Type type)
+    {
+        Node node = ScriptableObject.CreateInstance(type) as Node;
+        node.name = type.Name;
+        node.guid = GUID.Generate().ToString();
+        nodes.Add(node);
+
+        AssetDatabase.AddObjectToAsset(node, this);
+        AssetDatabase.SaveAssets();
+        return node;
+    }
+
+    public void DeleteNode(Node node)
+    {
+        nodes.Remove(node);
+        AssetDatabase.RemoveObjectFromAsset(node);
+        AssetDatabase.SaveAssets();
+    }
+
+    public void AddChild(Node parent, Node child)
+    {
+        DecoratorNode decorator = parent as DecoratorNode;
+        if (decorator)
+        {
+            decorator.child = child;
+        }
+
+        CompositeNode composite = parent as CompositeNode;
+        if (composite)
+        {
+            composite.children.Add(child);
+        }
+    }
+
+    public void RemoveChild(Node parent, Node child)
+    {
+        DecoratorNode decorator = parent as DecoratorNode;
+        if (decorator)
+        {
+            decorator.child = null;
+        }
+
+        CompositeNode composite = parent as CompositeNode;
+        if (composite)
+        {
+            composite.children.Remove(child);
+        }
+    }
+
+    public List<Node> GetChildren(Node parent)
+    {
+        List<Node> children = new List<Node>();
+
+        DecoratorNode decorator = parent as DecoratorNode;
+        if (decorator && decorator.child != null)
+        {
+           children.Add(decorator.child);
+        }
+
+        CompositeNode composite = parent as CompositeNode;
+        if (composite)
+        {
+            return composite.children;
+        }
+
+        return children;
+    }
+}
