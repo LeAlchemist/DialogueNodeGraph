9cf41dca114e5f8fa063c0f0d638f27d130a8307
Nicholas Ross <RossNicholasV@gmail.com>
2025-04-19 05:34:07 -0500

    Started Working on basic styling

--- Assets/Scripts/Editor/NodeView.cs
+++ Assets/Scripts/Editor/NodeView.cs
@@ -1,6 +1,8 @@
 using System;
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NodeView : UnityEditor.Experimental.GraphView.Node
 {
@@ -18,8 +20,36 @@ public class NodeView : UnityEditor.Experimental.GraphView.Node
         style.left = node.position.x;
         style.top = node.position.y;
 
+        //Import USS
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/Editor/Node.uss");
+        styleSheets.Add(styleSheet);
+
         CreateInputPorts();
         CreateOutputPorts();
+        SetupClasses();
+    }
+
+    private void SetupClasses()
+    {
+        switch (node)
+        {
+            case ActionNode:
+                AddToClassList("action");
+                AddToClassList(this.title);
+                break;
+            case CompositeNode:
+                AddToClassList("composite");
+                AddToClassList(this.title);
+                break;
+            case DecoratorNode:
+                AddToClassList("decorator");
+                AddToClassList(this.title);
+                break;
+            case RootNode:
+                AddToClassList("root");
+                AddToClassList(this.title);
+                break;
+        }
     }
 
     private void CreateInputPorts()
