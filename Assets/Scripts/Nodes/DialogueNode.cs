7ed04ab8516995c53c215626fe1b546ce3fff2e7
Nicholas Ross <RossNicholasV@gmail.com>
2025-04-20 01:56:18 -0500

    Begun prototyping Dialogue Node

---
+++ Assets/Scripts/Nodes/DialogueNode.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DialogueNode : ActionNode
+{
+    public DialogueNode()
+    {
+        description = "This holds dialogue";
+    }
+
+    protected override void OnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnStop()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override State OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
