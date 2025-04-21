a87ace6abdbefb5a6da7c7cfa9d65701678baa94
Nicholas Ross <RossNicholasV@gmail.com>
2025-04-21 14:16:00 -0500

    Update Node.cs

--- Assets/Scripts/Nodes/NodeTypes/Node.cs
+++ Assets/Scripts/Nodes/NodeTypes/Node.cs
@@ -23,29 +23,14 @@ public abstract class Node : ScriptableObject
     public Vector2 position;
     [HideInInspector]
     public string description;
-<<<<<<< Updated upstream:Assets/Scripts/Nodes/NodeTypes/Node.cs
     [HideInInspector]
     public Vector2 scale;
-=======
-<<<<<<< Updated upstream:Assets/Scripts/Nodes/Node.cs
-=======
-    [HideInInspector]
-    public Vector2 scale;
-    [HideInInspector]
-    public bool loop;
-    [HideInInspector]
-    public int loopCount;
->>>>>>> Stashed changes:Assets/Scripts/Nodes/Node.cs
 
     public Node()
     {
         scale.x = 250;
         scale.y = 750;
     }
-<<<<<<< Updated upstream:Assets/Scripts/Nodes/NodeTypes/Node.cs
-=======
->>>>>>> Stashed changes:Assets/Scripts/Nodes/NodeTypes/Node.cs
->>>>>>> Stashed changes:Assets/Scripts/Nodes/Node.cs
 
     public State Update()
     {
