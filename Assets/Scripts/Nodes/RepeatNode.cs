239f7b181baf72abe2f0ee942a773fe18e87583c
Nicholas Ross <RossNicholasV@gmail.com>
2025-04-21 04:22:03 -0500

    Repeat node can be set to amount
    You can now set the amount of times you want the repeat node to repeat the attached nodes

--- Assets/Scripts/Nodes/RepeatNode.cs
+++ Assets/Scripts/Nodes/RepeatNode.cs
@@ -1,5 +1,10 @@
+using UnityEngine;
+
 public class RepeatNode : DecoratorNode
 {
+    [Tooltip("If set to 0 will repeat non stop")]
+    public int repeatCount;
+    int timesRepeated;
     public RepeatNode()
     {
         description = "Plays the sequence of connected nodes over and over";
@@ -16,7 +21,19 @@ public class RepeatNode : DecoratorNode
 
     protected override State OnUpdate()
     {
-        child.Update();
+        if (repeatCount != 0)
+        {
+            timesRepeated++;
+            child.Update();
+            if (timesRepeated == repeatCount)
+            {
+                return State.Success;
+            }
+        }
+        else
+        {
+            child.Update();
+        }
         return State.Running;
     }
 }
