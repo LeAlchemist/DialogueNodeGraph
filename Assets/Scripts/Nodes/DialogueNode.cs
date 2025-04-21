80fffa9898fa2841c7c9d8280d88e249899845d5
Nicholas Ross <RossNicholasV@gmail.com>
2025-04-20 19:30:59 -0500

    Dialogue will now play based on word count and words per minute

--- Assets/Scripts/Nodes/DialogueNode.cs
+++ Assets/Scripts/Nodes/DialogueNode.cs
@@ -2,23 +2,64 @@ using UnityEngine;
 
 public class DialogueNode : ActionNode
 {
+    [TextArea]
+    public string dialogue;
+    public float wordsPerMinute = 100;
+    float secondsPerWord;
+    int wordCount, index;
+    [HideInInspector]
+    public float duration = 1;
+    [HideInInspector]
+    float startTime;
+
     public DialogueNode()
     {
         description = "This holds dialogue";
+        dialogue = "";
     }
 
     protected override void OnStart()
     {
-        throw new System.NotImplementedException();
+
     }
 
     protected override void OnStop()
     {
-        throw new System.NotImplementedException();
+
     }
 
     protected override State OnUpdate()
     {
-        throw new System.NotImplementedException();
+        secondsPerWord = 60 / wordsPerMinute;
+
+        GetWordCount();
+
+        duration = wordCount * secondsPerWord + 5;
+
+        if (Time.time - startTime > duration)
+        {
+            return State.Success;
+        }
+
+        return State.Running;
+    }
+
+    public void GetWordCount()
+    {
+        while (index < dialogue.Length && char.IsWhiteSpace(dialogue[index]))
+            index++;
+
+        while (index < dialogue.Length)
+        {
+            // check if current char is part of a word
+            while (index < dialogue.Length && !char.IsWhiteSpace(dialogue[index]))
+                index++;
+
+            wordCount++;
+
+            // skip whitespace until next word
+            while (index < dialogue.Length && char.IsWhiteSpace(dialogue[index]))
+                index++;
+        }
     }
 }
